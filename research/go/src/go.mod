53c519c05bc9d8b8fee85287134d5d3f1e097014
tianyx <tianyx@bhz.com.cn>
2021-10-09 17:41:18 +0800

    update

--- research/go/src/go.mod
+++ research/go/src/go.mod
@@ -1,3 +1,8 @@
 module ssd-servie
 
 go 1.16
+
+require (
+	github.com/tidwall/gjson v1.9.3 // indirect
+	github.com/tidwall/sjson v1.2.2 // indirect
+)
--- research/go/src/main.go
+++ research/go/src/main.go
@@ -1,11 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/tidwall/sjson"
+)
+
 // "errors"
-
-// "fmt"
-// "io"
-// "os"
-
 func main() {
 
+	ssdBytes, err := ioutil.ReadFile("ssd.json")
+	if err != nil {
+		// fmt.Println(err)
+		return
+	}
+	//fmt.Println(ssdString)
+
+	// value := gjson.GetBytes(ssdBytes, "a.b.e.2")
+	// if err != nil {
+	// 	// fmt.Println(err)
+	// 	return
+	// }
+	// //fmt.Println(reflect.TypeOf(value))
+	// fmt.Println(value)
+
+	dataBytes, err := ioutil.ReadFile("data.json")
+	if err != nil {
+		// fmt.Println(err)
+		return
+	}
+
+	ssdBytesNew, err := sjson.SetRawBytes(ssdBytes, "a.b.d.1", dataBytes)
+	if err != nil {
+		// fmt.Println(err)
+		return
+	}
+	fmt.Println(string(ssdBytesNew))
 }
